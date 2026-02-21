779d57297fd6c7699f61a50b0a957f6461e38b5b
vutii <giftmuuo.com@gmail.com>
2026-02-21 20:39:41 +0300

    update

--- app.py
+++ app.py
@@ -1,86 +1,139 @@
-# import flask and its component 
+# import flask and its components
 from flask import *
-# import the pymysql module - it helps create a connection bettween python flask and msql database
+import os
+# import the pymysql module - It helps us to create connection between python flask and mysql database
 import pymysql
 
-# create a flask application and give it a name
-app = Flask(__name__)
+# Create a flask application and give it a name
+app = Flask(__name__) 
 
+# configure the location to where your product images will be saved on your application
+app.config["UPLOAD_FOLDER"] = "static/images"
 
-# BELOW IS A SIGNUP ROUTE
-@app.route("/api/signup", methods =["POST"])
+# below is the sign up route 
+@app.route("/api/signup", methods = ["POST"])
 def signup():
     if request.method=="POST":
-        #extract the different details entered on postman form
+        # Extract the different details entered on the form 
         username = request.form["username"]
         email = request.form["email"]
         password = request.form["password"]
         phone = request.form["phone"]
 
-        #by use of the print function print all the details sent with the upcoming reques
+        # by use of the print function lets print all those details sent with the upcoming request
         # print(username, email, password, phone)
 
-        #establish a connection between flask/python and mysql
+        # establish a connection between flask/python and mysql
         connection = pymysql.connect(host="localhost", user="root", password="", database="sokogardenonline")
 
-
-        #create a cursor to execute the sqlqueries
+        # create a cursor to execute the sql queries
         cursor = connection.cursor()
 
-        #structure an sql to insert the details received on the form 
-        #%s is a placeholder-> it stands in places of actual values i.e we shall replace later on 
-        sql ="INSERT INTO users(username,email,phone,password) VALUES(%s,%s,%s,%s)"
+        # structure an sql to insert the details received from the form
+        # The %s is a placeholder -> A place holder it stands in places of actual values i.e we shall replace later on
+        sql = "INSERT INTO users(username,email,phone,password) VALUES(%s, %s, %s, %s)"
 
         # create a tuple that will hold all the data gotten from the form
         data = (username, email, phone, password)
 
-        # by use of the cursor , execute the sql as you replace the placeholder with actual values
-        cursor.execute(sql,data)
+        # by use of the cursor, execute the sql as you replace the placeholder with the actual values
+        cursor.execute(sql, data)
 
-        #commit the changes to the database 
+        # commit the changes to the database
         connection.commit()
 
 
-        return jsonify({"message": "User registered successfully"})
-#below is the login/signin route 
-@app.route("/api/signin", methods=["POST"])    
+        return jsonify({"message" : "User registered successfully"})
+
+
+
+# Below is the login/sign in route
+@app.route("/api/signin",methods=["POST"])
 def signin():
-    if request.method == "POST":
-        #extracct the to details entered on the form
+    if request.method =="POST":
+        # extract the two details entered on the form
         email = request.form["email"]
         password = request.form["password"]
 
-        # print out the details
-        #print(email, password)
+        # print out the details enteres
+        # print(email, password)
 
-        #create / establish a connecttion  to the database
+        # create/establish  a connection to the database
         connection = pymysql.connect(host="localhost", user="root", password="", database="sokogardenonline")
-        # create a cursor 
+
+        # create a cursor
         cursor = connection.cursor(pymysql.cursors.DictCursor)
 
-        #structure the sql query that will check hether the passord and email entered are correct
-        sql="SELECT * FROM users WHERE email = %s AND password = %s"
+        # structure the sql query that will check whether the email and password entered is correct
+        sql ="SELECT * FROM users WHERE email = %s AND password = %s"
 
-        # PUT THE DATA RECEIVED  INTO A TUPLE
-        data =(email, password)
+        # put the data received into a tuple
+        data = (email, password)
+
         # by use of the cursor execute the sql
         cursor.execute(sql, data)
-        #check whether there row returned and store the same varable 
-        count = cursor.rowcount
-        #print(count)
 
-        # if there are  rows return it mean the password  otherwise it means they are wrong
-        if count == 0:
-            return jsonify({"message": "login failed"})
-        else :
-            # there must be a user  we create  variable that will hold the details of the user fetched fromthe database
+        # check whether there are rows returned and store the same on a variable
+        count= cursor.rowcount
+        # print(count) 
+
+        # if there are record return it means the password and email are correct otherwise it means they are wrong 
+        if count== 0:
+            return jsonify({"message":"Login failed"}) 
+        else:
+            # There must be a user so we create a variable that will hold the details of the user fetched from the database
             user=cursor.fetchone()
-            #return the details to the content as ell as a message
-            return jsonify({"message" : "user logged in successfully", "user":user})
+            # Return the details to the frontend as well as a message
+            return jsonify({"message":"User logged in successfully", "user":user})
         
+# below is the route for adding products
+@app.route("/api/add_product", methods = ["POST"])
+def Addproduct():
+    if request.method == "POST":
+        # Extract the different details entered on the form
+        product_name = request.form["product_name"]
+        product_description = request.form["product_description"]
+        product_cost = request.form["product_cost"]
+        # for the product photo we shall fetch it from files as shown below
+        product_photo = request.files["product_photo"]
+
+        # extract the filename of the produc photo
+        filename = product_photo.filename
+        # print("This is the file name: ", filename)
+        # by use of the os module (operating system) we can extract the file path where the images is currently saved
+        photo_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+
+        # print("This is the photo path: ", photo_path)
+        # save the product photo image into the new location
+        product_photo.save(photo_path)
+
+        # print them out to test whether younare recieving the details sent with the request
+        # print(product_name, product_description, product_cost, product_photo)
+
+        # establish a connection between flask/python and mysql
+        connection = pymysql.connect(host="localhost", user="root", password="", database="sokogardenonline")
+
+        # create a cursor to execute the sql queries
+        cursor = connection.cursor()
+
+        # structure an sql to insert the details received from the form
+        # The %s is a placeholder -> A place holder it stands in places of actual values i.e we shall replace later on
+        sql = "INSERT INTO product_details(product_name, product_description, product_cost, product_photo) VALUES(%s, %s, %s, %s)"
+
+        # create a tuple that will hold all the data gotten from the form which are currently held onto the different variables declared above
+        data = (product_name, product_description, product_cost, filename)
+        # by use of the cursor, execute the sql as you replace the placeholder with the actual values
+        cursor.execute(sql, data)
+
+        # commit the changes to the database
+        connection.commit()
 
 
 
+        return jsonify({"message": "Product added successfully"})
 
+
+
+ 
 # run the application 
 app.run(debug=True)
